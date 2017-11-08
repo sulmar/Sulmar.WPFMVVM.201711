ee1aab638cd004d8f8bca29c65e867bee0ff7bc3
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2017-11-08 11:45:56 +0100

    Dodanie konwertera

---
+++ Sulmar.WPFMVVM.ShopPracz.WPFClient/Converters/SegmentStateToBrushConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace Sulmar.WPFMVVM.ShopPracz.WPFClient.Converters
+{
+    public class SegmentStateToBrushConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Models.SegmentState state = (Models.SegmentState) value;
+
+            switch(state)
+            {
+                case Models.SegmentState.FailureA: return new SolidColorBrush(Colors.Orange);
+                case Models.SegmentState.FailureB: return new SolidColorBrush(Colors.Red);
+
+                default: return new SolidColorBrush(Colors.Green);
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
