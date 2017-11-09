382de6774fe1721206bc90837aa67735a02e1bb2
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2017-11-09 14:36:17 +0100

    Dodanie ShellView

---
+++ Sulmar.WPFMVVM.ShopPracz.ViewModels/ShellViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sulmar.WPFMVVM.ShopPracz.ViewModels
+{
+    public class ShellViewModel : BaseViewModel
+    {
+
+       
+
+
+    }
+}
