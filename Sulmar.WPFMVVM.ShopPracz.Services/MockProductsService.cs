bf154604a40ce854fcfbd9a7686dbbdfc4b650e3
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2017-11-09 16:10:39 +0100

    Wyświetlenie produktów

---
+++ Sulmar.WPFMVVM.ShopPracz.Services/MockProductsService.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Sulmar.WPFMVVM.ShopPracz.Models;
+using System.Linq;
+
+namespace Sulmar.WPFMVVM.ShopPracz.Services
+{
+    public class MockProductsService : IProductsService
+    {
+        private ICollection<Product> products;
+
+        public MockProductsService()
+        {
+            products = new List<Product>
+            {
+                new Product { Id = 1, Color = "Red", Name = "Produkt 1", Price = 99.9m },
+                new Product { Id = 2, Color = "Blue", Name = "Produkt 2", Price = 199.9m },
+                new Product { Id = 3, Color = "Blue", Name = "Produkt 3", Price = 49.9m },
+                new Product { Id = 4, Color = "Green", Name = "Produkt 4", Price = 89.2m },
+                new Product { Id = 5, Color = "Red", Name = "Produkt 5", Price = 19.9m },
+            };
+        }
+
+        public void Add(Product product)
+        {
+            product.Id = products.Max(p => p.Id) + 1;
+
+            products.Add(product);
+        }
+
+        public ICollection<Product> Get()
+        {
+            return products;
+        }
+
+
+        public Product Get(int id) => products.Single(p => p.Id == id);
+
+        public Product Get(string name)
+        {
+            // jeśli będzie więcej niż 1 to rzuci wyjątkiem
+            return products.Single(p => p.Name == name);
+
+            // jeśli będzie więcej niż 1 to zwraca pierwszy(!) element
+            // return products.First(p => p.Name == name);
+
+        }
+
+        public void Remove(int id)
+        {
+            var product = Get(id);
+
+            products.Remove(product);
+        }
+
+        public void Update(Product product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Sulmar.WPFMVVM.ShopPracz.ViewModels/ProductsViewModel.cs
+++ Sulmar.WPFMVVM.ShopPracz.ViewModels/ProductsViewModel.cs
@@ -1,12 +1,90 @@
-﻿using System;
+﻿using Sulmar.WPFMVVM.Common4;
+using Sulmar.WPFMVVM.ShopPracz.Models;
+using Sulmar.WPFMVVM.ShopPracz.Services;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace Sulmar.WPFMVVM.ShopPracz.ViewModels
 {
     public class ProductsViewModel : BaseViewModel
     {
+
+        private ICollection<Product> products;
+        public ICollection<Product> Products
+        {
+            get { return products; }
+            set
+            {
+                products = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public Product SelectedProduct { get; set; }
+
+
+        private readonly IProductsService productsService;
+
+        public ProductsViewModel(IProductsService productsService)
+        {
+            this.productsService = productsService;
+
+            Load();
+        }
+
+        public ProductsViewModel()
+            : this(new MockProductsService())
+        {
+            
+        }
+
+        private void Load()
+        {
+            Products = new ObservableCollection<Product>(productsService.Get());
+        }
+
+        public ICommand AddCommand
+        {
+            get
+            {
+                return new RelayCommand(p => Add());
+            }
+        }
+
+        private void Add()
+        {
+            // TODO: Show ProductView
+            var product = new Product { Name = "Produkt 1", Color = "Red", Price = 99.9m };
+
+            productsService.Add(product);
+            this.Products.Add(product);
+        }
+
+        public ICommand RemoveCommand
+        {
+            get
+            {
+                return new RelayCommand(p => Remove(), p => CanRemove());
+            }
+        }
+
+        private void Remove()
+        {
+            productsService.Remove(SelectedProduct.Id);
+            this.Products.Remove(SelectedProduct);
+        }
+
+        private bool CanRemove()
+        {
+            return IsSelectedProduct;
+        }
+
+        private bool IsSelectedProduct => SelectedProduct != null;
+      
     }
 }
